93c8c7ca381ef7ed9715ac5b522adcc13409c5ed
navas <gabrielnavas@email.com>
2023-09-09 15:37:29 -0300

    simple raw web socket implemented

---
+++ src/main/java/com/websocket/rawwebsocket/ServerWebSocketHandler.java
@@ -0,0 +1,40 @@
+package com.websocket.rawwebsocket;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.SubProtocolCapable;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class ServerWebSocketHandler extends TextWebSocketHandler implements SubProtocolCapable {
+
+    private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String request = message.getPayload();
+        String response = String.format("response from server to '%s'", HtmlUtils.htmlEscape(request));
+        session.sendMessage(new TextMessage(response));
+    }
+
+    @Override
+    public List<String> getSubProtocols() {
+        return Collections.singletonList("subprotocol.demo.websocket");
+    }
+}
