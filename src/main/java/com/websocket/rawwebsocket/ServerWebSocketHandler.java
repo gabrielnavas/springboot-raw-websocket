41e40fcb9df527d159730361f8fa7cf09725078e
navas <gabrielnavas@email.com>
2023-09-09 16:39:59 -0300

    add Scheduled on send periodic message and logger after connection established and closed and handle text message methods

--- src/main/java/com/websocket/rawwebsocket/ServerWebSocketHandler.java
+++ src/main/java/com/websocket/rawwebsocket/ServerWebSocketHandler.java
@@ -1,28 +1,37 @@
 package com.websocket.rawwebsocket;
 
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.SubProtocolCapable;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.util.HtmlUtils;
 
+import java.io.IOException;
+import java.time.LocalTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.logging.Logger;
 
+@Component
+@EnableScheduling
 public class ServerWebSocketHandler extends TextWebSocketHandler implements SubProtocolCapable {
+
+    private static final Logger logger = Logger.getLogger(ServerWebSocketHandler.class.getName());
 
     private final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        logger.info(String.format("server connection opened %s", session.getId()));
         sessions.add(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        logger.info(String.format("server connection closed %s", session.getId()));
         sessions.remove(session);
     }
 
@@ -31,6 +40,17 @@ public class ServerWebSocketHandler extends TextWebSocketHandler implements SubP
         String request = message.getPayload();
         String response = String.format("response from server to '%s'", HtmlUtils.htmlEscape(request));
         session.sendMessage(new TextMessage(response));
+    }
+
+    @Scheduled(fixedRate = 5000)
+    void sendPeriodicMessage() throws IOException {
+        for(WebSocketSession session : sessions) {
+            if(session.isOpen()) {
+                String broadcast = String.format("server periodic message %s", LocalTime.now().toString());
+                logger.info(String.format("broadcast: %s", broadcast));
+                session.sendMessage(new TextMessage(broadcast));
+            }
+        }
     }
 
     @Override
